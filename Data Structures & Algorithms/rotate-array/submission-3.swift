14bf45053f6ee4a269e6d076537f9209990528f0
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-05-21 22:33:48 -0400

    Add: rotate-array - submission-3

---
+++ Data Structures & Algorithms/rotate-array/submission-3.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        let n = nums.count
+        let k = k % n
+
+        func reverse(_ start: Int, _ end: Int) {
+            var start = start
+            var end = end
+            while start < end {
+                let temp = nums[start]
+                nums[start] = nums[end]
+                nums[end] = temp
+                start += 1
+                end -= 1
+            }
+        }
+
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1) 
+    }
+}
