c30cc6e775c3043753c0da3464e76ef137296937
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-05-27 11:17:21 -0400

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        let closeToOpen: [Character: Character] = [")": "(", "]": "[", "}": "{"]
+        let sArray = Array(s)
+        var stack = [Character]()
+
+        for c in sArray {
+            if let open = closeToOpen[c] {
+                if stack.last == open {
+                    stack.removeLast()
+                } else {
+                    return false
+                }
+            } else {
+                stack.append(c)
+            }
+        }
+        
+        return stack.isEmpty
+    }
+}
