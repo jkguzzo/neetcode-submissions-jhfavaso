2dd0c348ed4f333c7a4198b0c566626a5b746ab4
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-05-27 10:41:25 -0400

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.swift
@@ -0,0 +1,27 @@
+class MinStack {
+    private var stack = [Int]()
+    private var minStack = [Int]()
+
+    init() {
+
+    }
+
+    func push(_ val: Int) {
+        stack.append(val)
+        let minVal = min(val, minStack.last ?? val)
+        minStack.append(minVal)
+    }
+
+    func pop() {
+        stack.removeLast()
+        minStack.removeLast()
+    }
+
+    func top() -> Int {
+        return stack.last!
+    }
+
+    func getMin() -> Int {
+        return minStack.last!
+    }
+}
