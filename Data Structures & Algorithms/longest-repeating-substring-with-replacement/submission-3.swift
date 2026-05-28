c7f178a77f6e3c42f7343cda95381e276b9d6b72
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-05-28 10:28:03 -0400

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func characterReplacement(_ s: String, _ k: Int) -> Int {
+        var l = 0
+        var res = 0
+        var count = [Character: Int]()
+        let sArray = Array(s)
+        for r in 0..<sArray.count {
+            count[sArray[r], default: 0] += 1
+            while (r - l + 1) - (count.values.max() ?? 0) > k {
+                count[sArray[l], default: 0] -= 1
+                l += 1
+            }
+            res = max(res, r - l + 1)
+        }
+        return res
+    }
+}
