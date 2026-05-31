2b1d1d9dcaf441bff663daf88e5fe9c63b150de6
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-05-31 15:47:54 -0400

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func maxSlidingWindow(_ nums: [Int], _ k: Int) -> [Int] {
+        var output = [Int]()
+        var deque = [Int]()
+        var l = 0
+        var r = 0
+        while r < nums.count {
+            while !deque.isEmpty && nums[deque.last!] < nums[r] {
+                deque.removeLast()
+            }
+            deque.append(r)
+
+            if l > deque.first! {
+                deque.removeFirst()
+            }
+
+            if (r + 1) >= k {
+                output.append(nums[deque.first!])
+                l += 1
+            }
+            r += 1
+        }
+        return output
+    }
+}
