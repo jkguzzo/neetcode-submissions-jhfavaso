16cfa80667d70a303899c28b2c819cfe44f8badf
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-06-10 19:44:05 -0400

    Add: top-k-elements-in-list - submission-14

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-14.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+        var count = [Int: Int]()
+        var freq = [[Int]](repeating: [], count: nums.count + 1)
+
+        for num in nums {
+            count[num, default: 0] += 1
+        }
+
+        for (num, count) in count {
+            freq[count].append(num)
+        }
+
+        var res = [Int]()
+        for i in stride(from: freq.count - 1, to: -1, by: -1) {
+            for c in freq[i] {
+                res.append(c)
+            }
+            if res.count == k {
+                return res
+            }
+        }
+        return res
+    }
+}
