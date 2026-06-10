9335b39f20c54d17e4bd78c6247980dba7fc15e6
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-06-10 19:35:01 -0400

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var res = [[Int]: [String]]()
+
+        for s in strs {
+            var count = [Int](repeating: 0, count: 26)
+            for c in s {
+                count[Int(c.asciiValue! - 97)] += 1
+            }
+            res[count, default: []].append(s)
+        }
+        return Array(res.values)
+    }
+}
