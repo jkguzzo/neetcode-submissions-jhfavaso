251e46d2672a13790abc201847d8fccebcaa7f61
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-06-13 15:28:23 -0400

    Add: depth-of-binary-tree - submission-11

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-11.swift
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        var stack: [(TreeNode?, Int)] = [(root, 1)]
+        var res = 0
+
+        while !stack.isEmpty {
+            let (node, depth) = stack.removeLast()
+            if let node = node {
+                res = max(res, depth)
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
+            }
+        }
+        return res
+    }
+}
