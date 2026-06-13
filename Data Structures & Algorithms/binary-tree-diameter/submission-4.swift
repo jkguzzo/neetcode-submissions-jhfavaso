e5ec7e80fb55ab7cb26788abd41798c06ad92cdd
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-06-13 15:30:03 -0400

    Add: binary-tree-diameter - submission-4

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-4.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+        var res = 0
+
+        func dfs(_ root: TreeNode?) -> Int {
+            guard let root = root else { return 0 }
+
+            let left = dfs(root.left)
+            let right = dfs(root.right)
+
+            res = max(res, left + right)
+            return 1 + max(left, right)
+
+        }
+
+        dfs(root)
+        return res
+    }
+}
