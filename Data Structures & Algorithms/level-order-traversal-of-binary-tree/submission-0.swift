dd7dd17f3e0b4fb2bd603fb468840b492d4eaeef
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-06-13 23:42:21 -0400

    Add: level-order-traversal-of-binary-tree - submission-0

---
+++ Data Structures & Algorithms/level-order-traversal-of-binary-tree/submission-0.swift
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+import DequeModule
+class Solution {
+    func levelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else { return [] }
+        var res = [[Int]]()
+        var q: Deque = [root]
+
+        while !q.isEmpty {
+            var curr = [Int]()
+            for _ in 0..<q.count {
+                let node = q.removeFirst()
+                curr.append(node.val)
+                if let left = node.left {
+                    q.append(left)
+                }
+                if let right = node.right {
+                    q.append(right)
+                }
+            }
+            res.append(curr)
+        }
+        return res
+    }
+}
