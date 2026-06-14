6cd23689de2f4f6a561ab88f42ce1d06debb0b5f
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-06-13 23:54:07 -0400

    Add: binary-tree-right-side-view - submission-0

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-0.swift
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+import DequeModule
+class Solution {
+    func rightSideView(_ root: TreeNode?) -> [Int] {
+        guard let root = root else { return [] }
+        var res = [Int]()
+        var q: Deque = [root]
+
+        while !q.isEmpty {
+            var level = [Int]()
+            for _ in 0..<q.count {
+                let node = q.removeFirst()
+                level.append(node.val)
+                if let left = node.left {
+                    q.append(left)
+                }
+                if let right = node.right {
+                    q.append(right)
+                }
+            }
+            res.append(level.last!)
+        }
+        return res
+    }
+}
+
+
+
+
+
