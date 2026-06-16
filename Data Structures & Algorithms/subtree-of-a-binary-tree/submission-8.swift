0df333830e78dbaaec8e61b8ac594d46f1bc7668
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-06-16 17:57:36 -0400

    Add: subtree-of-a-binary-tree - submission-8

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-8.swift
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func isSubtree(_ root: TreeNode?, _ subRoot: TreeNode?) -> Bool {
+        if subRoot == nil {
+            return true
+        }
+        if root == nil {
+            return false
+        }
+        if isSameTree(root, subRoot) {
+            return true
+        }
+        return isSubtree(root?.left, subRoot) || isSubtree(root?.right, subRoot)
+        
+        func isSameTree(_ root: TreeNode?, _ subRoot: TreeNode?) -> Bool {
+            if root == nil && subRoot == nil {
+                return true
+            }
+
+            if root == nil || subRoot == nil || root?.val != subRoot?.val {
+                return false
+            }
+            return isSameTree(root?.left, subRoot?.left) && isSameTree(root?.right, subRoot?.right)
+        }
+    }
+}
