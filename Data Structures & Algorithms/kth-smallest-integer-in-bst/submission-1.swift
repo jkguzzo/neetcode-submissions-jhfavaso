fffb6d926850a5b5479856e717cfd05ba27253c7
Julia Guzzo <111455100+jkguzzo@users.noreply.github.com>
2026-06-22 15:52:12 -0400

    Add: kth-smallest-integer-in-bst - submission-1

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-1.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func kthSmallest(_ root: TreeNode?, _ k: Int) -> Int {
+        var res = [Int]()
+
+        func dfs(_ root: TreeNode?) {
+            guard let root else { return }
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        }
+        dfs(root)
+        return res[k - 1]
+    }
+}
